a9b901a52eb252cbf2d6f299af2c345a5d717f85
td457 <td457@cam.ac.uk>
2022-11-27 12:32:44 +0000

    cython lt 3

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ VERSION = "0.1.0"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    "numpy<1.19.0", "jupyter", "matplotlib==3.2.0", "Cython<0.29",
+    "numpy<1.19.0", "jupyter", "matplotlib==3.2.0", "Cython<3",
 ]
 
 # What packages are optional?
