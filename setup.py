47671213e7180b8d779a597bd431c903bdc8e2c1
td457 <td457@cam.ac.uk>
2022-11-30 22:22:18 +0000

    added msnox to reqs

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ VERSION = "0.1.0"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    "pandas", "numpy", "jupyter", "matplotlib==3.2.0",
+    "pandas", "numpy", "jupyter", "matplotlib", "msnox"
 ]
 
 # What packages are optional?
