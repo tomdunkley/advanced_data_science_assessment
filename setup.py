e2e92c3951c5067f683d62e65490227cfe6d507a
td457 <td457@cam.ac.uk>
2022-12-01 01:56:55 +0000

    changed yaml to pyyaml

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ VERSION = "0.1.0"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    "pandas", "numpy", "jupyter", "matplotlib", "geopandas", "osmnx", "PyMySQL", "ipython-sql", "yaml", "statsmodels",
+    "pandas", "numpy", "jupyter", "matplotlib", "geopandas", "osmnx", "PyMySQL", "ipython-sql", "PyYAML", "statsmodels",
     "sqlalchemy", "shapely"
 ]
 
